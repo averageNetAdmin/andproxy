4b48f7e9931a3efb2cda1379beb49060e3d3b932
averageNetAdmin <86299254+averageNetAdmin@users.noreply.github.com>
2022-05-01 17:25:40 +0300

    Create main.go

---
+++ bin/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/averageNetAdmin/andproxy/bin/cnfrd"
+)
+
+func main() {
+	handlers, err := cnfrd.ReadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, handler := range handlers {
+		go handler.Handle()
+	}
+	for {
+		time.Sleep(60 * time.Second)
+	}
+}
